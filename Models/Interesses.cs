8e3b50cc294034ec0a368c37cc9d745f0cf86707
Davidson Eustaquio <davideustaquio@gmail.com>
2020-06-01 21:38:23 -0300

    alteração Interesse

--- Models/Interesses.cs
+++ Models/Interesses.cs
@@ -2,16 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CaoLendario.Models;
 
 namespace CaoLendario.Models
 {
 
-        public class Interesse : Adotante
-        {
-            private DateTime data;
-            private Boolean adotado;
+    public class Interesse : Adotante
+    {
+        private DateTime data;
+        private Boolean adotado;
 
-            public DateTime Data { get; set; }
-            public Boolean Adotado { get; set; }
-        }
+        public DateTime Data { get; set; }
+        public Boolean Adotado { get; set; }
+        public int AnimalID { get; set; }
+        public Animal Animal { get; set; }
+        public int AdotanteID { get; set; }
+        public Adotante Adotante { get; set; }
+    }
 }
+    
+
